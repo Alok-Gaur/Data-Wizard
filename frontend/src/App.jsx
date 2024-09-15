762bbac68c86dc915a50fb3e7bed145a2d6f1c03
rathoraryann <arrathor1334@gmail.com>
2024-09-15 15:06:12 +0530

    resume inputs components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BasicDetailsForResume from "./components/createResume/BasicDetailsForResume";
+import EducationDetails from "./components/createResume/EducationDetails";
+import Skills from "./components/createResume/Skills";
+import ProfileSummary from "./components/createResume/ProfileSummary";
 
+function App() {
   return (
     <>
       <h1>Data Wizard</h1>
+      {/* Use BrowserRouter instead of Router */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BasicDetailsForResume />} />
+          <Route path="/education-details" element={<EducationDetails />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/profile-summary" element={<ProfileSummary />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/createResume/ProfileSummary.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import { Controller, useForm } from 'react-hook-form';
+import { Box, Button } from '@mui/material';
+
+const ProfileSummary = () => {
+    const { handleSubmit, control } = useForm();
+    const [submittedData, setSubmittedData] = useState(''); 
+
+    
+    const submit = (data) => {
+        setSubmittedData(data.content); 
+    };
+
+    return (
+        <>
+            <div className='w-full'>
+                <form onSubmit={handleSubmit(submit)}>
+                    <label className='inline-block mb-1 pl-1'>Profile Summary</label>
+                    <Controller
+                        name="content"
+                        control={control}
+                        render={({ field: { onChange } }) => (
+                            <Editor
+                                apiKey='8bwiy3dhzy1rrbe453cbg6ajcj0f47srdvi9a2as6byucwog'
+                                initialValue=""
+                                init={{
+                                    initialValue: "",
+                                    height: 500,
+                                    menubar: true,
+                                    plugins: [
+                                        "advlist",
+                                        "autolink",
+                                        "lists",
+                                        "link",
+                                        "charmap",
+                                        "preview",
+                                        "anchor",
+                                        "searchreplace",
+                                        "visualblocks",
+                                        "code",
+                                        "fullscreen",
+                                        "insertdatetime",
+                                        "table",
+                                        "code",
+                                        "help",
+                                        "wordcount",
+                                        "anchor",
+                                    ],
+                                    toolbar:
+                                        "undo redo | blocks | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+                                    content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+                                }}
+                                onEditorChange={onChange}
+                            />
+                        )}
+                    />
+                    <Button type='submit' variant='contained' color='primary'>Submit</Button>
+                </form>
+
+                
+                {submittedData && (
+                    <Box mt={2} p={2} border={1} borderColor="grey.300">
+                        <h3>Submitted Profile Summary:</h3>
+                        <div dangerouslySetInnerHTML={{ __html: submittedData }} />
+                    </Box>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default ProfileSummary;
